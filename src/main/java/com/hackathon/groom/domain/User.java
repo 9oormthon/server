48a963a55e3931404eaaca80c23b77311f831308
jihwan <sgh002400@naver.com>
2022-08-25 00:04:20 +0900

    feat : 유저 엔티티 생성

--- src/main/java/com/hackathon/groom/domain/User.java
+++ src/main/java/com/hackathon/groom/domain/User.java
@@ -4,10 +4,7 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Getter
 @Entity
@@ -17,4 +14,10 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private int years;
 }
